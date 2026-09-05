b09e113c6acd6318f62663b34376a512eed039b5
Guilherme Pinheiro <guilherme.pinheirops@gmail.com>
2026-09-05 14:40:35 -0300

    Atualizacao para solicitar instalacao

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ciclo-mob-v38';
+const CACHE='ciclo-mob-v39';
 const META_CACHE='cycleseed-meta';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/favicon.svg'];
 
