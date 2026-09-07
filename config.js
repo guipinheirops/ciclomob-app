c4445ada4fff27e402d774e453ef76d34ca169f2
Guilherme Pinheiro <guilherme.pinheirops@gmail.com>
2026-09-07 14:03:31 -0300

    Alteracao para remover mensagens obsoletas

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 window.CYCLESEED_CONFIG = {
   SUPABASE_URL: 'https://heglcvgpverqfmqpdyok.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_jkZYWhDMKTbq6hKph4Ml-g_PKrzkUJd',
-  // Será configurada na etapa de Web Push.
+  // Chave pública usada para as notificações.
   VAPID_PUBLIC_KEY: 'BPJPG3RH9lSPMkFjHKYF8JM4AHZLJajmXnvMBy62D_oKVstlDNk3iwpubRExVzqWR4P0oboMFU184jYdE0kxVFA',
   // Será configurada quando publicarmos a Edge Function de convites por e-mail.
   INVITE_EMAIL_FUNCTION: ''
